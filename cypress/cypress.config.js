ba9921bab6b23e175aa20a74845dd763db0afbcb
betaniaAmaral <betaniajonas@gmail.com>
2025-01-25 08:27:49 -0300

    att configurações do projeto

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,9 +2,23 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://automationpractice.pl',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: false,
+    baseUrl: 'http://automationpractice.pl', 
+    viewportWidth: 1280, 
+    viewportHeight: 720, 
+    retries: {
+      runMode: 2,
+      openMode: 0, 
+    },
+    defaultCommandTimeout: 10000, 
+    responseTimeout: 15000, 
+    video: false, 
+    env: {
+  
+      loginPath: '/index.php?controller=authentication&back=my-account',
+      dashboardPath: '/index.php?controller=my-account',
+    },
+    setupNodeEvents(on, config) {
+      return config;
+    },
   },
 });

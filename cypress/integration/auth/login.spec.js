791a6f5822b48ab779ae5504e34e013f9494ceb4
betaniaAmaral <betaniajonas@gmail.com>
2025-01-25 08:21:11 -0300

    add fixtures validUser e invalidUser

--- cypress/integration/auth/login.spec.js
+++ cypress/integration/auth/login.spec.js
@@ -1,17 +1,1 @@
-describe('Autenticação - Cenário 1', () => {
-  beforeEach(() => {
-    cy.visit('/index.php?controller=authentication&back=my-account');
-  });
 
-  it('Deve permitir login com credenciais válidas', () => {
-    // Carrega os dados do fixture
-    cy.fixture('users').then((users) => {
-      // Usa o comando customizado para login com dados do fixture
-      cy.login(users.validUser.email, users.validUser.password);
-    });
-
-    // Validação do redirecionamento
-    cy.url().should('include', 'controller=my-account');
-    cy.contains('Welcome to your account').should('be.visible');
-  });
-});

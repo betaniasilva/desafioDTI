fdea9aea75935b97c8774e25905baf4f06106432
betaniaAmaral <betaniajonas@gmail.com>
2025-01-25 09:04:20 -0300

    att commands para cobrir cenarios de testes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,12 @@
 Cypress.Commands.add('login', (email, password) => {
+  cy.get('#email').type(email); 
+  cy.get('#passwd').type(password); 
+  cy.get('#SubmitLogin').click(); 
+});
+
+Cypress.Commands.add('recoverPassword', (email) => {
+  cy.contains('Forgot your password?').click(); 
+  cy.url().should('include', 'controller=password'); 
   cy.get('#email').type(email);
-  cy.get('#passwd').type(password);
-  cy.get('#SubmitLogin').click();
+  cy.get('button[name="submitIdentity"]').click(); 
 });
